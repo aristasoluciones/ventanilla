22a52cca7c30ab45175db2a13c16792bcfee3b7d
hbkruzpe <isc061990@outlook.com>
2022-05-20 12:48:33 -0500

    cambios

--- pg/formulario/form_finalizacion.php
+++ pg/formulario/form_finalizacion.php
@@ -1,24 +1,23 @@
-<div class="row"
-     x-show="controlActaFinal">
-    <div class="col-md-12 text-center"
-         x-show="loading_download_acta">
-        <div class='text-center'><i class='fas fa-1x fa-sync fa-spin'></i></div>
+<div class="row">
+    <!-- listar actas firmadas -->
+    <div class="col-md-12" x-show="listaActaFirmadaConciliacion.length">
+        <label>Lista de actas disponibles para descargar</label><br>
+        <div class="btn-group">
+            <template x-for="(item, index) in listaActaFirmadaConciliacion">
+                <a key="index" type="button" :href="path_file() + item.path_acta_firmada" class="btn btn-outline-info" target="_blank" title="Descargar acta">
+                    <span x-text="item.nombre"></span> <i class="fa fa-download"></i>
+                </a>
+            </template>
+        </div>
     </div>
-    <div class="col-md-12"
-         x-show="!loading_download_acta">
-        <div class="form-group" >
-            <div class="btn-group">
-                <button
-                        @click="descargarActaFinal()"
-                        class="btn btn-success">
-                    Descargar acta <i class="fa fa-download"></i>
-                </button>
-                <button
-                        @click="descargarActaFinal(1)"
-                        class="btn btn-warning">
-                    Vista previa <i class="fa fa-search-plus"></i>
-                </button>
-            </div>
+    <div class="col-md-12" x-show="listaActaFirmadaResolucion.length">
+        <label>Lista de actas disponibles para descargar</label><br>
+        <div class="btn-group">
+            <template x-for="(item, index) in listaActaFirmadaConciliacion">
+                <a key="index" type="button" :href="path_file() + item.path_acta_firmada" class="btn btn-outline-info" target="_blank" title="Descargar acta">
+                    <span x-text="item.nombre"></span> <i class="fa fa-download"></i>
+                </a>
+            </template>
         </div>
     </div>
 </div>
