7bbc6a67e7c06252de34d2c5265e200425e78dc3
hbkruzpe <isc061990@outlook.com>
2022-07-12 13:47:10 -0500

    cambios

--- php/clase_querys.php
+++ php/clase_querys.php
@@ -123,7 +123,7 @@ class QuerysB
                   (SELECT fn_calcularVigenciaVentanilla(2, null, ssa.fecha_registro) 
                        FROM tbl_solicitud_queja_seguimiento ssa
                        WHERE ssa.id_solicitud_queja = a.id_solicitud_queja order by ssa.id_solicitud_queja_seguimiento desc limit 1) as fecha_vencimiento_etapa, 
-                      (SELECT count(*) FROM tbl_solicitud_queja_recurso sr WHERE sr.id_solicitud_queja=a.id_solicitud_queja) existe_recurso,
+                      (SELECT count(*) FROM tbl_solicitud_queja_recurso sr WHERE sr.id_solicitud_queja=a.id_solicitud_queja AND sr.origen = 2) existe_recurso,
                   IF(a.id_etapa_queja IN(9, 17), 1, 0) finalizado
                   FROM (select sa.*, sb.tipo, sb.nombre nombre_manifestacion from tbl_solicitud_queja sa join tblc_tipo_queja sb on sa.id_tipo_queja=sb.id_tipo_queja)  a
                   LEFT JOIN tblc_municipio b ON a.id_municipio_hecho = b.id_municipio
